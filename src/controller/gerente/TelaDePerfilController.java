a3ab3df8ec2f7065b870ee6e69610b1f88d83380
bielzindoback <134566731+bielzindoback@users.noreply.github.com>
2023-11-02 17:40:28 -0300

    Update TelaDePerfilController.java

--- src/controller/gerente/TelaDePerfilController.java
+++ src/controller/gerente/TelaDePerfilController.java
@@ -1,21 +1,9 @@
 package controller.gerente;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
-import model.Endereco;
-import model.Funcionario;
-import model.dao.ConexãoBD;
-import model.dao.Gerente.TelaDePerfilDao;
-import model.dao.login.MainModel;
-
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -24,13 +12,31 @@ import java.util.List;
 
 import controller.Main;
 import controller.Ferramentas.EfeitoBtn;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import model.Endereco;
+import model.Funcionario;
+import model.LoginModel;
+import model.PerfilGeralModel;
+import model.dao.ConexãoBD;
+import model.dao.Gerente.TelaDePerfilDao;
+import model.dao.login.LoginDAO;
+import model.dao.login.MainModel;
 
 public class TelaDePerfilController {
 
 	EfeitoBtn efeito = new EfeitoBtn();
 
     @FXML
-    private ImageView BtnEncerrarSessao;
+    private Button BtnEncerrarSessao;
 
     @FXML
     private TextField TxtBairro;
@@ -67,17 +73,29 @@ public class TelaDePerfilController {
 
     @FXML
     private Label lblTrocarFoto;
+   
+    private LoginDAO ld;
+    private LoginModel lm; // Você deve definir o objeto LoginModel com o ID do usuário logado
+	private TelaDePerfilDao perfilGeralDAO; // Você deve ter uma classe PerfilGeralDAO para gerenciar o perfil
+	private Connection conex;
+	private PerfilGeralModel pgm;
+	private int idFuncionario;
 
+	 public TelaDePerfilController() throws SQLException {
+	        conex = ConexãoBD.Conexao(); // Certifique-se de que conex seja inicializado corretamente aqui
+	        pgm = new PerfilGeralModel();
+	        perfilGeralDAO = new TelaDePerfilDao(conex, pgm);
+	    }
  
     @FXML
     private void initialize() throws SQLException {
         int idFuncionario = MainModel.verificaID();
         List<Funcionario> funcionarios = TelaDePerfilDao.getFuncionarioComEndereco(idFuncionario);
-        
-        
+
         if (!funcionarios.isEmpty()) {
             Funcionario funcionario = funcionarios.get(0); // Supondo que você obtenha apenas um funcionário
             funcionario.setId(idFuncionario);
+         
             TxtNome.setText(funcionario.getNomeCompleto());
             TxtCPF.setText(funcionario.getCpf());
             TxtDataNascimento.setText(funcionario.getDataNascimento().toString());
@@ -94,19 +112,40 @@ public class TelaDePerfilController {
         }
     }
     
-    @FXML
-    void trocaFoto(MouseEvent event) {
-        // Configurar o FileChooser para filtrar arquivos de imagem
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().add(new ExtensionFilter("Imagens", "*.jpg", "*.png", "*.jpeg"));
 
-        // Mostrar o diálogo de seleção de arquivo
-        File selectedFile = fileChooser.showOpenDialog(new Stage());
-        if (selectedFile != null) {
-            imgFoto.setImage(new Image(selectedFile.toURI().toString()));     
-        }
-    }
-    
+	@FXML
+	void trocaFoto(MouseEvent event) throws IOException {
+		// Configurar o FileChooser para filtrar arquivos de imagem
+				FileChooser fileChooser = new FileChooser();
+				fileChooser.getExtensionFilters().add(new ExtensionFilter("Imagens", "*.jpg", "*.png", "*.jpeg"));
+
+				// Mostrar o diálogo de seleção de arquivo
+				File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+	    if (selectedFile != null) {
+	        try {
+	            // Ler os dados da imagem como um array de bytes
+	            File file = new File(selectedFile.toURI());
+	            FileInputStream inputStream = new FileInputStream(file);
+	            byte[] imageData = new byte[(int) file.length()];
+	            inputStream.read(imageData);
+	            inputStream.close();
+
+	            // Atualizar a imagem exibida na tela
+	            Image novaImagem = new Image(selectedFile.toURI().toString());
+	            imgFoto.setImage(novaImagem);
+	            // Atualizar a imagem no banco de dados
+	            pgm.setId(idFuncionario); // Defina o ID do funcionário
+	            pgm.setFotoPerfil(imageData);
+	            if(perfilGeralDAO.salvarFotoPerfil(pgm)==true) {
+	            	System.out.println("SALVOU");
+	            }
+	  	        } catch (Exception e) {
+	            e.printStackTrace();
+	            // Trate as exceções aqui, se necessário.
+	        }
+	    }
+	}
    
 
     @FXML
@@ -119,13 +158,13 @@ public class TelaDePerfilController {
     @FXML
     void EncerrarSessao(MouseEvent event) throws Exception {
 
-    	
+    	perfilGeralDAO.salvarFotoPerfil(pgm);
+		perfilGeralDAO.carregarFoto(idFuncionario);
     	Connection conecao = ConexãoBD.Conexao();
     	String updateSql ="UPDATE sessao SET sessao = ? WHERE id = 1";
         PreparedStatement updatePs = conecao.prepareStatement(updateSql);
         updatePs.setInt(1, 0);
         int lf = updatePs.executeUpdate();
-        
         updatePs.close();
         conecao.close();
         
@@ -186,6 +225,17 @@ public class TelaDePerfilController {
     	BtnEncerrarSessao.setEffect(null);
     	
     }
+    
+    public void setIdFuncionario(int idFuncionario) {
+	    this.idFuncionario = idFuncionario;
+
+	    // Após definir o ID do funcionário, carregue a imagem do banco de dados
+	    byte[] imagemData = perfilGeralDAO.carregarFoto(idFuncionario);
+	    if (imagemData != null) {
+	        Image imagem = new Image(new ByteArrayInputStream(imagemData));
+	        imgFoto.setImage(imagem);
+	    }
+	}
     
     
 }
