36f24385abc20059101aaff6bd923625e883d764
bielzindoback <134566731+bielzindoback@users.noreply.github.com>
2023-11-06 08:11:45 -0400

    Update TelaDeCadastro2Controller.java

--- src/controller/login/TelaDeCadastro2Controller.java
+++ src/controller/login/TelaDeCadastro2Controller.java
@@ -1,220 +1,241 @@
 package controller.login;
 
-	import java.sql.Connection;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
 
-import model.dao.ConexãoBD;
-import model.dao.login.RecuperSenhaModel;
+
+
+
 import controller.Ferramentas.EfeitoBtn;
 import controller.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-	import javafx.scene.control.TextField;
-	import javafx.scene.image.ImageView;
-	import javafx.scene.input.KeyEvent;
-	import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
-	public class TelaRecuperaSenhaController {
 
-		EfeitoBtn efeito = new EfeitoBtn();
-		
-		 @FXML
-		    private PasswordField ConfirmarSen;
+public class TelaDeCadastro2Controller {
+	
+	
+	public static String email,Senha,txtr1,txtr2,txtr3;
+	
+	EfeitoBtn efeito = new EfeitoBtn();
 
-		    @FXML
-		    private ImageView EscondeSenha;
-
-		    @FXML
-		    private ImageView EscondeSenha2;
-
-		    @FXML
-		    private ImageView MostraSenha;
-
-		    @FXML
-		    private ImageView MostraSenha2;
-
-		    @FXML
-		    private Line linha1;
-
-		    @FXML
-		    private Line linha2;
-
-		    @FXML
-		    private ImageView logo;
-
-		    @FXML
-		    private Pane paneColuna;
-
-		    @FXML
-		    private Pane panePrincipal;
-
-		    @FXML
-		    private Pane paneTeto;
-
-		    @FXML
-		    private Label redefinirSenha;
-
-		    @FXML
-		    private PasswordField senha;
-
-		    @FXML
-		    private TextField txfApelido;
-
-		    @FXML
-		    private TextField txfCPF;
-
-		    @FXML
-		    private TextField txfNomeMae;
-
-		    @FXML
-		    private TextField txfPrimeiraEscola;
-
-		    @FXML
-		    private Text txtApelido;
-
-		    @FXML
-		    private Text txtCPF;
-
-		    @FXML
-		    private Text txtConfirmarSen;
-
-		    @FXML
-		    private Text txtNomeMae;
-
-		    @FXML
-		    private Text txtNovaSenha;
-
-		    @FXML
-		    private Text txtPrimeiraEscola;
-		    
-		    @FXML
-		    private Button buttonFinalizar;
-		    
-	    @FXML
-	    void BtnFinalizar(MouseEvent event) throws SQLException {
-	    	
-	    	if(txtPrimeiraEscola.getText() !=""&& txtNomeMae.getText() !=""
-    			&& txtApelido.getText() !=""&& txtCPF.getText() != "" &&
-    			ConfirmarSen.getText() != "" && senha.getText() != ""){
-	    		
-	    		if(senha.getText().equals(ConfirmarSen.getText())) {
-	    			
-	    			
-	    			RecuperSenhaModel.AlteraSenha(senha.getText(), txtPrimeiraEscola.getText()
-	    					, txtNomeMae.getText(), txtApelido.getText());
-	    			
-	    			
-
-	            } else JOptionPane.showMessageDialog(null, "senha digitada no campo de confirmação não é igual a senha digitada no campo senha");
-	    			
-	    			
-	    			
-	    		} else JOptionPane.showMessageDialog(null, "prencha todos os campos");
-	    		
-	    		
-	    		
-	    	}
-	    	
-	    @FXML
-	    void EscondeSenhaSenha(MouseEvent event) {
-
-	    	txtNovaSenha.setVisible(false);
-	       	 String Senha = String.valueOf(senha.getText());
-	       	MostraSenha.setVisible(true);
-	        MostraSenha.setDisable(false);
-	        
-	        EscondeSenha.setVisible(false);
-	        EscondeSenha.setDisable(true);
-	       	 txtNovaSenha.setText(Senha);
-	           senha.setVisible(true);
-	    	
-	    }
+	 @FXML
+	    private Line LinhaHorizontal;
 
 	    @FXML
-	    void EscondeSenhaSenha1(MouseEvent event) {
-
-	    	txtConfirmarSen.setVisible(false);
-	       	 String Senha = String.valueOf(senha.getText());
-	       	MostraSenha2.setVisible(true);
-	       	MostraSenha2.setDisable(false);
-	        
-	       	EscondeSenha2.setVisible(false);
-	       	EscondeSenha2.setDisable(true);
-	       	txtConfirmarSen.setText(Senha);
-	        ConfirmarSen.setVisible(true);
-	    	
-	    }
+	    private Line LinhaVertical;
 
 	    @FXML
-	    void MostrarSenha(MouseEvent event) {
-
-	    	String Senha = String.valueOf(senha.getText());
-
-	    	txtNovaSenha.setText(Senha);
-	    	txtNovaSenha.setVisible(true);
-	        MostraSenha.setVisible(false);
-	        MostraSenha.setDisable(true);
-	        
-	        EscondeSenha.setVisible(true);
-	        EscondeSenha.setDisable(false);
-	        
-	        senha.setVisible(false);
-	    	
-	    }
+	    private ImageView Logo;
 
 	    @FXML
-	    void MostrarSenha1(MouseEvent event) {
+	    private Button buttonFinalizar;
 
-	    	String Senha = String.valueOf(senha.getText());
+	    @FXML
+	    private ImageView imagemTijolos;
 
-	    	txtConfirmarSen.setText(Senha);
-	    	txtConfirmarSen.setVisible(true);
-	        MostraSenha2.setVisible(false);
-	        MostraSenha2.setDisable(true);
-	        
-	        EscondeSenha2.setVisible(true);
-	        EscondeSenha2.setDisable(false);
-	        
-	        ConfirmarSen.setVisible(false);
-	    	
-	    	
-	    }
+	    @FXML
+	    private Pane painelPrincipal;
 
+	    @FXML
+	    private Pane pane1;
+
+	    @FXML
+	    private Pane pane2;
+
+	    @FXML
+	    private PasswordField senha;
+
+	    @FXML
+	    private TextField txfApelido;
+
+	    @FXML
+	    private PasswordField ConfirmarSen;
+
+	    @FXML
+	    private TextField txfEmail;
+
+	    @FXML
+	    private TextField txfNomeMae;
+
+	    @FXML
+	    private TextField txfPrimeiraEscola;
+
+	    @FXML
+	    private Text txtApelido;
+
+	    @FXML
+	    private Label txtAviso;
+
+	    @FXML
+	    private Label txtCadastro;
+
+	    @FXML
+	    private Text txtConfirmarSen;
+
+	    @FXML
+	    private Text txtEmail;
+
+	    @FXML
+	    private Text txtNomeMae;
+
+	    @FXML
+	    private Text txtPrimeiraEscola;
+
+	    @FXML
+	    private Text txtSenha;
+
+	    @FXML
+	    private ImageView MostraSenha;
 	    
-
 	    @FXML
-	    void Entrar1(MouseEvent event) {
-
-	    	buttonFinalizar.setEffect(efeito.Efeito());
-	    	
-	    }
-
-
+	    private ImageView EscondeSenha;
+	    
 	    @FXML
-	    void Sair1(MouseEvent event) {
-
-	    	buttonFinalizar.setEffect(null);
-	    	
-	    }
-
+	    private ImageView MostraSenha2;
+	    
 	    @FXML
-	    void vol1(MouseEvent event) throws Exception {
-	    	
-	    	Main.Cena("Login");
-	    	
-	    }
+	    private ImageView EscondeSenha2;
+    
+   
+    @FXML
+    void Continuar1(MouseEvent event) throws Exception {
+    	//verifica se todos os campos estão prenchidos
+    	if(senha.getText() != "" && txtEmail.getText() != "" && ConfirmarSen.getText() != "" 
+    			&& txfPrimeiraEscola.getText() !=""&& txfNomeMae.getText() !=""
+    			&& txfApelido.getText() !="") {
+    		
+    		//verifica se a senha digitada no campo de senha é igual ao campo confirma senha
+    		
+    		if(senha.getText().equals( ConfirmarSen.getText())) {
+    			
+    			email=txtEmail.getText();
+    			Senha=senha.getText();
+    			txtr1=txfPrimeiraEscola.getText();
+    			txtr2=txfNomeMae.getText();
+    			txtr3=txfApelido.getText();
+    			
+    			senha.setText("");
+    			txtEmail.setText("");
+    			ConfirmarSen.setText("");
+    			txfPrimeiraEscola.setText("");
+    			txfNomeMae.setText("");
+    			txfApelido.setText("");
+    			
+    			Main.Cena("Cadastro2");
+    		
+    		
+    		}else JOptionPane.showMessageDialog(null, "senha digitada no campo de confirmação não é igual a senha digitada no campo senha ");
+    	
+    	}else JOptionPane.showMessageDialog(null, "Preencha todos os campos");
+    	
 
-	}
+    }
+
+
+    @FXML
+    void clique(MouseEvent event) throws Exception{
+
+    	Main.Cena("Login");
+    	
+    }
+    
+    
+
+    @FXML
+    void EscondeSenhaSenha(MouseEvent event) {
+
+    	 txtSenha.setVisible(false);
+       	 String Senha = String.valueOf(senha.getText());
+       	MostraSenha.setVisible(true);
+        MostraSenha.setDisable(false);
+        
+        EscondeSenha.setVisible(false);
+        EscondeSenha.setDisable(true);
+       	 txtSenha.setText(Senha);
+           senha.setVisible(true);
+    	
+    }
+
+    @FXML
+    void EscondeSenhaSenha2(MouseEvent event) {
+
+    	 txtConfirmarSen.setVisible(false);
+       	 String Senha = String.valueOf(senha.getText());
+       	MostraSenha2.setVisible(true);
+        MostraSenha2.setDisable(false);
+        
+        EscondeSenha2.setVisible(false);
+        EscondeSenha2.setDisable(true);
+       	 txtSenha.setText(Senha);
+           ConfirmarSen.setVisible(true);
+    	
+    }
+
+    @FXML
+    void MostrarSenha(MouseEvent event) {
+
+
+    	String Senha = String.valueOf(senha.getText());
+
+    	txtSenha.setText(Senha);
+    	txtSenha.setVisible(true);
+        MostraSenha.setVisible(false);
+        MostraSenha.setDisable(true);
+        
+        EscondeSenha.setVisible(true);
+        EscondeSenha.setDisable(false);
+        
+        senha.setVisible(false);
+    	
+    	
+    }
+
+    @FXML
+    void MostrarSenha2(MouseEvent event) {
+    
+
+    	String Senha = String.valueOf(senha.getText());
+
+    	txtConfirmarSen.setText(Senha);
+    	txtConfirmarSen.setVisible(true);
+    	MostraSenha2.setVisible(false);
+    	MostraSenha2.setDisable(true);
+        
+    	EscondeSenha2.setVisible(true);
+    	EscondeSenha2.setDisable(false);
+        
+        ConfirmarSen.setVisible(false);
+    	
+    	
+    }
+    
+    
+  
+    
+    @FXML
+    void Entrar1(MouseEvent event) {
+    	
+    	buttonFinalizar.setEffect(efeito.Efeito());
+    	
+    }
+
+    @FXML
+    void Sair1(MouseEvent event) {
+    	
+    	buttonFinalizar.setEffect(null);
+    }
+
+
+}
+
 
 	
 	
